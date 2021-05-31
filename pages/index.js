87fe74654568c539efe6f80316e344b1e8b3dd81
franpb14 <franperejonbarrios@gmail.com>
2021-05-31 21:27:10 +0200

    cambiado los mensajes de titulo del modal y el boton para cerrarlo

--- pages/index.js
+++ pages/index.js
@@ -137,7 +137,7 @@ const Post = () => {
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLongTitle">Plan de inversiones</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -159,7 +159,7 @@ const Post = () => {
         </PricingTable>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
       </div>
     </div>
   </div>
